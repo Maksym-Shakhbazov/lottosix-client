66a7c3100d807d2ca21c9a51881cd4967550b8d0
maksym_shakhbazov <M2x1vass>
2016-09-19 13:12:42 +0300

    Initial commit

---
+++ app/app.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import {Http, Response} from '@angular/http';
+import 'rxjs/Rx';
+
+@Component({
+    selector: 'my-app',
+    templateUrl: 'views/index.html',
+})
+
+export class AppComponent {
+    private url = '//ec2-52-59-230-187.eu-central-1.compute.amazonaws.com/api/time/';
+    public time;
+
+    constructor(private http: Http) {
+        this.http = http;
+        this.getTime();
+    }
+
+    getTime(){
+
+        return this.http.get(this.url)
+            .map((res:Response) => res.json())
+            .subscribe(
+                data => {
+                    this.time = new Date(data.date || 0)
+                },
+                err => console.error(err),
+                () => console.log('done')
+            );
+    }
+
+}
